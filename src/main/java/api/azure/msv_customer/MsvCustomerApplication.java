dfd3de3c8c1cd8c5a7cf34bac024b637857dea15
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-09-21 23:58:11 -0500

    first commit

---
+++ src/main/java/api/azure/msv_customer/MsvCustomerApplication.java
@@ -0,0 +1,13 @@
+package api.azure.msv_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/azure/msv_customer/MsvCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.azure.msv_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
