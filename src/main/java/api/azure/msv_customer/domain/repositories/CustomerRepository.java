b1c3fe201a7fd667270bdee3a770d1295824216b
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2024-09-22 01:43:31 -0500

    Add CustomerRespository

--- src/main/java/api/azure/msv_customer/domain/repositories/CustomerRepository.java
+++ src/main/java/api/azure/msv_customer/domain/repositories/CustomerRepository.java
@@ -1,2 +1,9 @@
-package api.azure.msv_customer.domain.repositories;public class CustomerRepository {
+package api.azure.msv_customer.domain.repositories;
+
+import api.azure.msv_customer.domain.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 }
